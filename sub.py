1a74b3b4a3768741d76ee65764b056a4a328b730
aashish <aashishkarkix@gmail.com>
2026-07-31 09:45:33 +0545

    feat: sub

---
+++ sub.py
@@ -0,0 +1,11 @@
+def sub(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    print("sub =", sub(a, b))
